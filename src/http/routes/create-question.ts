80f8a7f6781496fcb2cd59d0e0c273471a13aa81
Davi Félix <davifelix@usp.br>
2025-07-10 00:01:37 -0300

    fix: finding chunks and genrating answer

--- src/http/routes/create-question.ts
+++ src/http/routes/create-question.ts
@@ -1,7 +1,9 @@
+import { and, eq, sql } from "drizzle-orm";
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import z from "zod/v4";
 import { db } from "../../db/connection.ts";
 import { schema } from "../../db/schema/index.ts";
+import { generateAnswer, generateEmbeddings } from "../../services/gemini.ts";
 
 export const createQuestionRoute: FastifyPluginAsyncZod = async (app) => {
 	app.post(
@@ -20,11 +22,41 @@ export const createQuestionRoute: FastifyPluginAsyncZod = async (app) => {
 			const { question } = req.body;
 			const { roomId } = req.params;
 
+			const embeddings = await generateEmbeddings(question);
+
+			const embeddingsVectorString = `[${embeddings.join(",")}]`;
+
+			const chunks = await db
+				.select({
+					id: schema.audioChunks.id,
+					transcription: schema.audioChunks.transcription,
+					similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsVectorString}::vector)`,
+				})
+				.from(schema.audioChunks)
+				.where(
+					and(
+						eq(schema.audioChunks.roomId, roomId),
+						sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsVectorString}::vector) > 0.7`,
+					),
+				)
+				.orderBy(
+					sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingsVectorString}::vector)`,
+				)
+				.limit(3);
+
+			let answer: string | null = null;
+
+			if (chunks.length > 0) {
+				const transcriptions = chunks.map((c) => c.transcription);
+				answer = await generateAnswer(question, transcriptions);
+			}
+
 			const result = await db
 				.insert(schema.questions)
 				.values({
 					roomId,
 					question,
+					answer,
 				})
 				.returning();
 
@@ -32,7 +64,7 @@ export const createQuestionRoute: FastifyPluginAsyncZod = async (app) => {
 				throw new Error("Failed to create question");
 			}
 
-			return res.status(201).send({ questionId: result[0].id });
+			return res.status(201).send({ questionId: result[0].id, answer });
 		},
 	);
 };
--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -47,3 +47,39 @@ export const generateEmbeddings = async (text: string) => {
 
 	return response.embeddings[0].values;
 };
+
+export const generateAnswer = async (
+	question: string,
+	transcriptions: string[],
+) => {
+	const context = transcriptions.join("\n\n");
+
+	const prompt = `
+		Com base no texto fornecido abaixo como contexto, responda a pergunta de forma clara e precisa em português do Brasil.
+
+		CONTEXTO:
+		${context}
+
+		QUESTION:
+		${question}
+
+		INTRUÇÕES:
+		- Use apenas informações fornecidas no contexto enviado;
+		- Se a resposta não for encontrada no contexto, apenas responda que não possui informações suficientes para responder;
+		- Seja objetivo;
+		- Mantenha um tom educativo e profissional;
+		- Cite trechos relevates do contexto se apropriado;
+		- Caso cite o contexto, refira-se a esse contexto usando o termo "conteúdo da aula".
+	`;
+
+	const response = await gemini.models.generateContent({
+		model: transcriptionModel,
+		contents: [{ text: prompt }],
+	});
+
+	if (!response.text) {
+		throw new Error("Failed to generate response");
+	}
+
+	return response.text;
+};
